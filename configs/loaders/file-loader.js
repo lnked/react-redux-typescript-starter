980c1994e5015d09c27996dbb08b1b0b09aa0886
Edik Bulikyan <ed.proff@gmail.com>
2019-02-15 12:53:24 +0300

    improve configs

---
+++ configs/loaders/file-loader.js
@@ -0,0 +1,11 @@
+const { cacheDirectory } = require('./options');
+
+module.exports = ({ prefix, ...props }) => {
+  return {
+    loader: 'file-loader',
+    options: {
+      ...props,
+      name: `${prefix}/[hash].[ext]`
+    }
+  };
+}
---
+++ configs/loaders/url-loader.js
@@ -0,0 +1,12 @@
+const { cacheDirectory } = require('./options');
+
+module.exports = ({ prefix, limit, ...props }) => {
+  return {
+    loader: 'url-loader',
+    options: {
+      ...props,
+      limit,
+      name: `${prefix}/[hash].[ext]`
+    }
+  };
+}
