a4bf1f3791ebe32ae4f314d9137da83f57996dd5
Edik Bulikyan <ed.proff@gmail.com>
2019-02-17 01:39:55 +0300

    update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,12 +2,3 @@ module.exports = {
   "**/*.{ts,tsx,js,jsx}": ["tslint --fix", "git add"],
   "**/*.{json,md,scss,yaml,yml}": ["git add"],
 };
-
-// "lint-staged": {
-//   "*.{js,jsx}": [
-//     "git add"
-//   ],
-//   "*.{json,md,scss,yaml,yml}": [
-//     "git add"
-//   ]
-// },
