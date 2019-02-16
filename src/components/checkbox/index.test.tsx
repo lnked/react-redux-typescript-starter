fde66402e00f77e7afdb1af1f49908bcd689910a
Edik Bulikyan <ed.proff@gmail.com>
2019-02-17 00:42:34 +0300

    update

--- src/components/checkbox/index.test.tsx
+++ src/components/checkbox/index.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
-import { CheckboxWithLabel } from '../checkboxWithLabel'
+import { CheckboxWithLabel } from './'
 
 test('CheckboxWithLabel changes the text after click', () => {
   const checkbox = shallow(<CheckboxWithLabel labelOn='On' labelOff='Off' />)
