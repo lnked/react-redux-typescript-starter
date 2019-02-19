7bb9dba8244ec5ca2f6f6672179b8c355ec72d00
Edik Bulikyan <ed.proff@gmail.com>
2019-02-19 04:30:07 +0300

    upgrade types

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,3 +1,10 @@
 // declare global {
 //   type unknown = { [key: string]: any } | object | number | string | boolean | symbol | undefined | null | void
 // }
+
+declare var process: {
+  env: {
+    [key: string]: string | undefined;
+    PUBLIC_URL: string;
+  }
+}
