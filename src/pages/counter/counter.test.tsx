1cf3f6649cf029f52ed7df39c0a051f8b2751e0f
Edik Bulikyan <ed.proff@gmail.com>
2019-02-20 13:29:26 +0300

    update

--- src/pages/counter/counter.test.tsx
+++ src/pages/counter/counter.test.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import renderer from 'react-test-renderer'
+// import { act } from 'react-dom/test-utils'
+// https://github.com/threepointone/react-act-examples/blob/master/act-examples.test.js
 
 import Counter from './'
 
@@ -11,3 +13,30 @@ describe('<Counter />', () => {
     done()
   })
 })
+
+// it("should increment a counter", () => {
+//   const el = document.createElement("div");
+//   document.body.appendChild(el);
+//   // we attach the element to document.body to ensure events work
+//   ReactDOM.render(<App />, el);
+//   const button = el.childNodes[0];
+//   for (let i = 0; i < 3; i++) {
+//     button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   }
+//   expect(button.innerHTML).toBe("3");
+// });
+
+// it("should render 1", () => {
+//   const el = document.createElement("div");
+//   act(() => {
+//     ReactDOM.render(<App />, el);
+//   });
+//   expect(el.innerHTML).toBe("1"); // this passes!
+// });
+
+// act(() => {
+//   for (let i = 0; i < 3; i++) {
+//     button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+//   }
+// });
+// expect(button.innerHTML).toBe(3); // this fails, it's actually "1"!
