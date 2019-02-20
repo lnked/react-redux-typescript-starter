6f21590857fce2a7107b8d138d51d7c62559bfbe
Edik Bulikyan <ed.proff@gmail.com>
2019-02-20 03:33:56 +0300

    update tests

--- src/pages/counter/counter.test.tsx
+++ src/pages/counter/counter.test.tsx
@@ -1,27 +1,13 @@
-// src/components/__tests__/counter_spec.tsx
 import * as React from 'react'
-// import * as TestUtils from 'react-dom/test-utils'
-import * as ShallowRenderer from 'react-test-renderer/shallow'
-
-// import { createStore } from 'redux'
+import renderer from 'react-test-renderer'
 
 import Counter from './'
-// import { reducers } from '../../reducers'
+
+const tree: any = renderer.create(<Counter initialCount={3} />).toJSON()
 
 describe('<Counter />', () => {
-  it('renders', () => {
-    // const store = createStore(reducers)
-    const renderer = ShallowRenderer.createRenderer()
-
-    // <Counter store={store} />
-
-    expect(renderer.render(<Counter initialCount={3} />)).toMatchSnapshot()
-  })
-
-  it('t-1', () => {
-    const renderer = ShallowRenderer.createRenderer()
-    renderer.render(<Counter initialCount={3} />)
-    const result: any = renderer.getRenderOutput()
-    expect(result.type.target).toBe('div')
+  it('t-1', done => {
+    expect(tree.type).toBe('div')
+    done()
   })
 })
--- src/pages/counter/index.tsx
+++ src/pages/counter/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 
 export interface P {
-  initialCount: number;
+  initialCount?: number;
 }
 
 const StyledCounter = styled.div`
