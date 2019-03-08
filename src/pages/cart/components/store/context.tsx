737d1a23f680ca1517cb1c1132416c82c0ba90d2
lnked <ed.proff@gmail.com>
2019-03-08 17:24:45 +0300

    update store

--- src/pages/cart/components/store/context.tsx
+++ src/pages/cart/components/store/context.tsx
@@ -10,10 +10,10 @@ export const { Provider, Consumer } = React.createContext(initialState)
 export const reducer = (state: any, action: any) => {
   const [ name, method ] = action.type.toLowerCase().split('/')
 
-  const functionName = `reducer${ucFirst(method || name)}`
+  const fn = `reducer${ucFirst(method || name)}`
 
-  if (typeof reducers[functionName] === 'function') {
-    return reducers[functionName].apply(null, [state, action])
+  if (typeof reducers[fn] === 'function') {
+    return reducers[fn].apply(null, [state, action])
   }
 
   return { ...state }
