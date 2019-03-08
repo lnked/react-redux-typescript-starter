42d250376ae459556b2e0a0870bed0a4fed6fa7e
lnked <ed.proff@gmail.com>
2019-03-08 15:04:24 +0300

    upgrade devendencies

--- src/pages/cart/components/store/context.tsx
+++ src/pages/cart/components/store/context.tsx
@@ -1,17 +1,16 @@
 import * as React from 'react'
 
+import { ucFirst } from 'utils'
 import * as reducers from './reducers'
 
 const initialState = {}
 
 export const { Provider, Consumer } = React.createContext(initialState)
 
-const ucfirst = (s: string) => `${s.charAt(0).toUpperCase()}${s.slice(1)}`
-
 export const reducer = (state: any, action: any) => {
   const [ name, method ] = action.type.toLowerCase().split('/')
 
-  const functionName = `reducer${ucfirst(method || name)}`
+  const functionName = `reducer${ucFirst(method || name)}`
 
   if (typeof reducers[functionName] === 'function') {
     return reducers[functionName].apply(null, [state, action])
