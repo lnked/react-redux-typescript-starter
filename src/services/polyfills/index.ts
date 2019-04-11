c20a609fa87d7a938eed77d98d51668ef2c57b60
lnked <ed.proff@gmail.com>
2019-04-12 02:36:10 +0300

    upgrade devendencies

--- src/services/polyfills/index.ts
+++
@@ -1,8 +0,0 @@
-// require('whatwg-fetch')
-// require('intl')
-// require('url-polyfill')
-// require('core-js/web/dom-collections')
-// require('core-js/es6/map')
-// require('core-js/es6/string')
-// require('core-js/es6/array')
-// require('core-js/es6/object')
