79122833c792bdba04e32303e82cca057c34d506
lnked <ed.proff@gmail.com>
2019-04-15 00:46:47 +0300

    add hooks example

---
+++ src/pages/todo/useInputState.ts
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const useInputState = (initialValue: string) => {
+  const [value, setValue] = useState(initialValue)
+
+  return {
+    value,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value)
+    },
+    reset: () => setValue('')
+  }
+}
+
+export default useInputState
