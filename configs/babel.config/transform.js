453572c19e76f433cfa0b1e1300cbb24140db5cc
lnked <ed.proff@gmail.com>
2019-04-21 03:13:46 +0300

    update transform

--- configs/babel.config/transform.js
+++ configs/babel.config/transform.js
@@ -1,15 +1,25 @@
 const findPath = require('./find-path');
+const changeCase = require('change-case');
+const { sourcePath } = require('../options');
 
 const transform = (prefix, preventFullImport) => {
   return {
     transform: (importName, matches) => {
-      return findPath(prefix, importName, (matches[1] || false))
+      const name = changeCase.kebabCase(importName)
+
+      if (!preventFullImport) {
+        const source = findPath(prefix, importName, (matches[1] || false))
+        return source.replace(`${sourcePath}/`, '');
+      }
+
+      return `${prefix}/${name}`
     },
     preventFullImport,
   };
 };
 
 module.exports = {
+  './components': transform('./components', true),
   '\.\.\/utils\/?(((\\w*)?\/?)*)': transform('utils', false),
   '\.\.\/theme\/?(((\\w*)?\/?)*)': transform('theme', false),
   '\.\.\/pages\/?(((\\w*)?\/?)*)': transform('pages', false),
