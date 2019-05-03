a1d356aa23a4cf7f056c22dd4b7d630878bfc3f6
lnked <ed.proff@gmail.com>
2019-05-03 09:52:28 +0300

    improve build

--- configs/plugins/common/index.js
+++ configs/plugins/common/index.js
@@ -2,7 +2,6 @@ const WebpackPlugins = require('./webpack-plugins');
 const DuplicatesPlugin = require('./duplicates-plugin');
 const CopyWebpackPlugin = require('./copy-webpack-plugin');
 const HtmlWebpackPlugin = require('./html-webpack-plugin');
-const PreloadWebpackPlugin = require('./preload-webpack-plugin');
 
 module.exports = {
   plugins: [
@@ -10,6 +9,5 @@ module.exports = {
     ...DuplicatesPlugin(),
     ...CopyWebpackPlugin(),
     ...HtmlWebpackPlugin(),
-    ...PreloadWebpackPlugin(),
   ].filter(Boolean),
 };
--- configs/plugins/production/index.js
+++ configs/plugins/production/index.js
@@ -5,6 +5,7 @@ const CleanWebpackPlugin = require('./clean-webpack-plugin');
 const WorkboxWebpackPlugin = require('./workbox-webpack-plugin');
 const BundleAnalyzerPlugin = require('./webpack-bundle-analyzer');
 const ImageminWebpackPlugin = require('./imagemin-webpack-plugin');
+const PreloadWebpackPlugin = require('./preload-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('./script-ext-html-webpack-plugin');
 
 module.exports = {
@@ -16,6 +17,7 @@ module.exports = {
     ...WorkboxWebpackPlugin(),
     ...ImageminWebpackPlugin(),
     ...CompressionPlugin(),
+    ...PreloadWebpackPlugin(),
     ...BundleAnalyzerPlugin(),
   ].filter(Boolean),
 };
