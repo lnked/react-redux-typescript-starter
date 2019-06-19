08d1f8947f068e9b316087b53d050ec20a675d4f
Edik Bulikyan <ed.proff@gmail.com>
2019-06-19 11:34:08 +0300

    upgrade dependencies

---
+++ src/decorators/storage/index.ts
@@ -0,0 +1,13 @@
+const storage = ({ ...props }) => {
+  console.log(props);
+
+  // descriptor.value = () => {
+  //   // console.storage(`Calling "${name}" with`, arguments);
+  //   return value.apply(null, arguments);
+  // };
+
+  // return descriptor;
+  return null;
+};
+
+export default storage;
---
+++ src/utils/parse-query-string.ts
@@ -0,0 +1,15 @@
+const x = "Categories=All&Search=Filus";
+
+const parseQueryString = (str: string) => str.split('&').reduce((acc, el) => {
+  const [key, value] = el.split('=');
+  acc[key] = value;
+  return acc;
+}, {});
+
+const params = parseQueryString(x);
+
+console.log(params); // { Categories: "All", Search: "Filus" }
+
+const values = Object.values(params);
+
+console.log(values); // [ "All", "Filus" ]
