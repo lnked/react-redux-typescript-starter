c7fd40243d0ce89b7a7d938149f4980681ce4a4d
Edik Bulikyan <ed.proff@gmail.com>
2019-08-11 13:44:05 +0300

    Upgrade dependencies

--- configs/plugins/common/unused-files-webpack-plugin.js
+++ configs/plugins/common/unused-files-webpack-plugin.js
@@ -1,6 +1,12 @@
 const { UnusedFilesWebpackPlugin } = require('unused-files-webpack-plugin');
 
+const { analyze } = require('../../options');
+
 module.exports = () => {
+  if (!analyze) {
+    return []
+  }
+
   return [
     new UnusedFilesWebpackPlugin({
       patterns: ['src/**/*.*']
