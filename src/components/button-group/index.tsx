a1d5ee1bceaa88bf6f72399706509f854be83efd
Edik Bulikyan <ed.proff@gmail.com>
2019-08-17 06:15:44 +0300

    Upgrade dependencies

--- src/components/button-group/index.tsx
+++ src/components/button-group/index.tsx
@@ -3,13 +3,6 @@ import * as React from 'react';
 import { OuterProps } from './types';
 import { Group } from './styled';
 
-const ButtonGroup: React.FC<OuterProps> = (props: OuterProps) => {
-  // const itemReferer = React.createRef<HTMLInputElement>();
-  // const [count, setCount] = useState(value);
-
-  return (
-    <Group {...props} />
-  );
-};
+const ButtonGroup: React.FC<OuterProps> = props => <Group {...props} />;
 
 export default ButtonGroup;
