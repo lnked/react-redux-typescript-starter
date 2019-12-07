8038b19cf356d99aad48927db731f40ef7bd33b9
Edik Bulikyan <ed.proff@gmail.com>
2019-12-07 08:48:35 +0300

    Upgrade dependencies

--- src/components/preloadLink/index.tsx
+++ src/components/preloadLink/index.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Link } from './styled';
 
 export interface OuterProps {
+  to: string;
   label?: string;
   children?: React.ReactChild[] | string;
   component?: any;
@@ -10,62 +11,22 @@ export interface OuterProps {
 
 let timeout: any = null;
 
-// import { Link } from 'react-router-dom'
-// ...
-// const LinkWithPreload = ({ to, onPreload, ...rest }) => {
-//   return (
-//     <Link
-//       to={to}
-//       onMouseEnter={() => preloadRouteComponent(to)}
-//       {...rest}
-//     />
-//   );
-// };
-// export default LinkWithPreload;
-
 const PreloadLink: React.FC<OuterProps> = (props: OuterProps) => {
   const { label, children, component, ...restProps } = props;
 
-  console.log('timeout: ', timeout);
-
   const handleMouseEnter = () => {
-    console.log('mouse enter')
     clearTimeout(timeout);
-    console.log('component && component.preload: ', component, component.preload)
+
     timeout = setTimeout(() =>
       component && component.preload && component.preload()
     , 250);
   }
 
   return (
-    <Link {...restProps} onMouseEnter={handleMouseEnter}>
+    <Link onMouseEnter={handleMouseEnter} {...restProps}>
       {children || label}
     </Link>
   );
 }
-
-// class PreloadLink extends React.Component<OuterProps, void> {
-//   timeout: any = null;
-
-//   handleMouseEnter = () => {
-//     const { component } = this.props;
-
-//     clearTimeout(this.timeout);
-
-//     this.timeout = setTimeout(() =>
-//       component && component.preload && component.preload()
-//     ,                         250);
-//   }
-
-//   render () {
-//     const { label, children, component, ...props } = this.props;
-
-//     return (
-//       <Link {...props} onMouseEnter={this.handleMouseEnter}>
-//         {children || label}
-//       </Link>
-//     );
-//   }
-// }
 
 export default PreloadLink;
--- src/utils/preloadComponent.tsx
+++ src/utils/preloadComponent.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
-import { matchPath } from "react-router-dom";
 
-const findComponentForRoute = (path: string, routes: any) => {
-  const matchingRoute = routes.find((route: any) =>
-    matchPath(path, {
-      path: route.path,
-      exact: route.exact
-    })
-  );
-  return matchingRoute ? matchingRoute.component : null;
-};
+// import { matchPath } from "react-router-dom";
 
-export const preloadRouteComponent = (path: string) => {
-  const component = findComponentForRoute(path, routes);
-  if (component && component.preload) {
-    component.preload();
-  }
-};
+// const findComponentForRoute = (path: string, routes: any) => {
+//   const matchingRoute = routes.find((route: any) =>
+//     matchPath(path, {
+//       path: route.path,
+//       exact: route.exact
+//     })
+//   );
+//   return matchingRoute ? matchingRoute.component : null;
+// };
+
+// export const preloadRouteComponent = (path: string) => {
+//   const component = findComponentForRoute(path, routes);
+//   if (component && component.preload) {
+//     component.preload();
+//   }
+// };
 
 export const preloadComponent = (importStatement: any) => {
   const Component: any = React.lazy(importStatement);
