767971a9557073516813f738eb7c35b41a938ff9
lnked <ed.proff@gmail.com>
2020-05-10 03:29:57 +0300

    upgrade dependencies

--- src/hooks/useOnMount.ts
+++ src/hooks/useOnMount.ts
@@ -3,5 +3,5 @@ import { useEffect } from 'react';
 type EffectCallback = () => void | (() => void);
 
 export function useOnMount(onMount: EffectCallback) {
-  useEffect(onMount, []);
+  useEffect(onMount, [onMount]);
 }
