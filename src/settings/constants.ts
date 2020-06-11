75c3c014ec52af81b8f26b7f264ba44c1d0ccaba
lnked <ed.proff@gmail.com>
2020-06-11 10:15:04 +0300

    upgrade dependencies

--- src/settings/constants.ts
+++ src/settings/constants.ts
@@ -1,3 +1,7 @@
 import { local } from 'storage.io';
 
-export const SITE_LANGUAGE = local.get('language') || 'en';
+const language = window.navigator.language.slice(0, 2);
+
+export const STORE_UI = 'ui';
+export const STORE_APP = 'app';
+export const SITE_LANGUAGE = local.get('language') || language || 'en';
