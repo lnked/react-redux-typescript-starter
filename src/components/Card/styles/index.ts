a605ededd660d9878973ebd97ccfd494d82537f0
lnked <ed.proff@gmail.com>
2020-07-03 06:28:22 +0300

    upgrade dependencies

--- src/components/Card/styles/index.ts
+++ src/components/Card/styles/index.ts
@@ -4,6 +4,9 @@ import { CommonStyles } from 'themes';
 
 export const StyledDiv = styled.div`
   color: ${CommonStyles.themeColor};
+  border: 1px solid lime;
 `;
 
-export const StyledButton = styled.button``;
+export const StyledButton = styled.button`
+  color: #f00;
+`;
