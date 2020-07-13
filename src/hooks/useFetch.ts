1e7200ce0b2e07b0b2b7e2ecd02fbcaa97ecd2a5
lnked <ed.proff@gmail.com>
2020-07-13 15:30:17 +0300

    upgrade dependencies

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect, useRef } from "react";
+// This custom hook centralizes and streamlines handling of HTTP calls
+export default function useFetch(url, init) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const prevInit = useRef();
+  const prevUrl = useRef();
+
+  useEffect(() => {
+  // Only refetch if url or init params change.
+    if (prevUrl.current === url && prevInit.current === init) return;
+    prevUrl.current = url;
+    prevInit.current = init;
+    fetch(process.env.REACT_APP_API_BASE_URL + url, init)
+      .then(response => {
+        if (response.ok) return response.json();
+        setError(response);
+      })
+      .then(data => setData(data))
+      .catch(err => {
+        console.error(err);
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  }, [init, url]);
+
+  return { data, loading, error };
+}
+
+// import useFetch from "hooks/useFetch";
+
+// export default function HookDemo() {
+//   const { data, loading, error } = useFetch("users");
+//   if (loading) return "Loading...";
+//   if (error) return "Oops!";
+//   return data[0].username;
+// }
