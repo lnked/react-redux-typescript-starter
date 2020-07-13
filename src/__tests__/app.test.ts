1d4937f8679477eb763700f45eea84979a9775bd
lnked <ed.proff@gmail.com>
2020-07-13 17:05:07 +0300

    upgrade dependencies

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// // test('renders learn react link', () => {
+// //   const { getByText } = render();
+// //   const linkElement = getByText(/learn react/i);
+// //   expect(linkElement).toBeInTheDocument();
+// // });
+
+// // describe("<App />", () => {
+// //   it("Renders <App /> component correctly", () => {
+// //     const { getByText } = render(<App />);
+// //     expect(getByText(/Getting started with React testing library/i)).toBeInTheDocument();
+// //   });
+// // });
