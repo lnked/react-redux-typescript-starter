360cc6959359d8549aa1cd29823029ce00e14f24
lnked <ed.proff@gmail.com>
2020-10-06 22:26:14 +0300

    update

--- src/store/providers/index.ts
+++ src/store/providers/index.ts
@@ -2,5 +2,4 @@ export { UiStore } from './ui-store';
 export { AppStore } from './app-store';
 export { DataStore } from './data-store';
 export { ArrayStore } from './array-store';
-export { ObjectStore } from './object-store';
 export { SessionStore } from './session-store';
