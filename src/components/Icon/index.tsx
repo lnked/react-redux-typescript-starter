575fcc8e831ce151f81358f3230411bccb8762da
lnked <ed.proff@gmail.com>
2020-10-12 01:26:17 +0300

    update

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -10,3 +10,31 @@ export const Icon: React.FC<OuterProps> = (props: OuterProps) => {
 };
 
 Icon.displayName = 'Icon';
+
+
+// import * as React from 'react';
+// import SVG from 'react-inlinesvg';
+
+// export type Props = {
+//   type: any;
+// }
+
+// export const Icon: React.FC<Props> = ({ type, ...props }) => <SVG src={type} {...props} />;
+
+// Icon.displayName = 'Icon';
+
+// export default Icon;
+
+// {/* <SVG
+//   baseURL="/home"
+//   cacheRequests={true}
+//   description="The React logo"
+//   loader={<span>Loading...</span>}
+//   onError={error => console.log(error.message)}
+//   onLoad={(src, hasCache) => console.log(src, hasCache)}
+//   preProcessor={code => code.replace(/fill=".*?"/g, 'fill="currentColor"')}
+//   src="https://cdn.svgporn.com/logos/react.svg"
+//   title="React"
+//   uniqueHash="a1f8d1"
+//   uniquifyIDs={true}
+// /> */}
