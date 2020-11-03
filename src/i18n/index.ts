a1602e18fd9c3f342aa8a2790d65a14b6509efc2
lnked <ed.proff@gmail.com>
2020-11-04 00:13:58 +0300

    types

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,6 +4,7 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import { options } from './options';
+import type { Languages } from './types';
 
 i18n.use(LanguageDetector).use(initReactI18next);
 
@@ -11,14 +12,14 @@ if (!i18n.isInitialized) {
   i18n.init(options);
 }
 
-export const setLanguage = (lng: string) => i18n.changeLanguage(lng);
+export const setLanguage = (lng: Languages) => i18n.changeLanguage(lng);
 
 export const i18nInit = () => {
-  i18n.on('languageChanged', (lng: string) => local.set('language', lng));
+  i18n.on('languageChanged', (lng: Languages) => local.set('language', lng));
 };
 
 export const i18nUnload = () => {
-  i18n.off('languageChanged', (lng: string) => local.set('language', lng));
+  i18n.off('languageChanged', (lng: Languages) => local.set('language', lng));
 };
 
 export default i18n;
