623199feb60b1540fcdcba9de7ba9f911f23aa56
lnked <ed.proff@gmail.com>
2021-06-09 21:48:48 +0300

    upgrade dependencies

--- src/themes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Theme } from './Theme';
-
-export { Box } from './Box';
-export { Theme } from './Theme';
-export { Colors } from './Colors';
-export { CommonStyles } from './CommonStyles';
-
-export default Theme;
