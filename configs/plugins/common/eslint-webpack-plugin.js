01450ca3f15f26aac7f834ed00620919ad916fce
lnked <ed.proff@gmail.com>
2021-11-01 02:27:17 +0300

    fix configs

--- configs/plugins/common/eslint-webpack-plugin.js
+++ configs/plugins/common/eslint-webpack-plugin.js
@@ -5,12 +5,17 @@ const options = require('../../options');
 
 module.exports = () => {
   return [
+    new ESLintPlugin({
+      files: ['src/**/*.tsx?', 'src/**/*.jsx?'],
+      exclude: ['/node_modules/'],
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+    }),
     // new ESLintPlugin({
     //   cache: true,
     //   ignore: true,
     //   emitWarning: true,
     //   useEslintrc: true,
-    //   exclude: [`/node_modules/`],
+    //   exclude: ['/node_modules/'],
     //   extensions: ['js', 'jsx', 'ts', 'tsx'],
     //   eslintPath: resolve(options.rootPath, '.eslintrc.js'),
     //   resolvePluginsRelativeTo: __dirname,
